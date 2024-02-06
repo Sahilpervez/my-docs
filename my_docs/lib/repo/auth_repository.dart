55eba9242ed51190dc4897198edaa640649dc7a8
Kaami_sama <sahilpervez26122002@gmail.com>
2024-02-06 18:43:32 +0530

    ready to deploy on netlify

--- my_docs/lib/repo/auth_repository.dart
+++ my_docs/lib/repo/auth_repository.dart
@@ -63,14 +63,14 @@ class AuthRepository {
           headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json; charset=UTF-8',
-            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Origin": "https://my-docs-c8473.web.app",
             "Access-Control-Allow-Credentials": "true",
             "Access-Control-Allow-Headers":
                 "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale,x-auth-token",
             "Access-Control-Allow-Methods":
                 "GET, POST, PUT, PATCH, DELETE, OPTIONS"
           },
-          encoding: Encoding.getByName('br'),
+          encoding: Encoding.getByName('utf-8'),
         );
 
         switch (res.statusCode) {
